4ff180ef651f99968a992f81e50797a655893269
Rasutei <rustingheart@hotmail.com>
2021-08-20 17:15:25 -0300

    Update custom.js

--- custom.js
+++ custom.js
@@ -1,24 +1,25 @@
-console.log("Starting script.")
-var vw, vh, ratio
-var correctRatio = 16/9
-while (!document.getElementById("unity-canvas"))
-var canvas = document.getElementById("unity-canvas")
-while (!document.getElementById("unity-footer"))
-while (document.getElementById("unity-footer")) document.getElementById("unity-footer").remove();
-Update();
-setInterval(Update, 1000);
-;
-function Update(){
-  console.log("Updating.")
-  vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
-  vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0)
-  ratio = vw/vh
-  if (ratio <= correctRatio){
-    canvas.style.width = vw+"px"
-    canvas.style.height = vw*(1/correctRatio)+"px"
-  }
-  else{
-    canvas.style.height = vh+"px"
-    canvas.style.width = vh*correctRatio+"px"
+window.onload = Script;
+function Script(){
+  console.log("Starting script.")
+  var vw, vh, ratio
+  var correctRatio = 16/9
+  var canvas = document.getElementById("unity-canvas")
+  while (document.getElementById("unity-footer")) document.getElementById("unity-footer").remove();
+  Update();
+  setInterval(Update, 1000);
+  ;
+  function Update(){
+    console.log("Updating.")
+    vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
+    vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0)
+    ratio = vw/vh
+    if (ratio <= correctRatio){
+      canvas.style.width = vw+"px"
+      canvas.style.height = vw*(1/correctRatio)+"px"
+    }
+    else{
+      canvas.style.height = vh+"px"
+      canvas.style.width = vh*correctRatio+"px"
+    }
   }
 }
