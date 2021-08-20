d80f99bb662f5accde496649b4a89fcba1323a03
Rasutei <rustingheart@hotmail.com>
2021-08-20 17:26:06 -0300

    Update custom.js

--- custom.js
+++ custom.js
@@ -1,4 +1,3 @@
-document.addEventListener('DOMContentLoaded', Script, false);
 function Script(){
   console.log("Starting script.")
   var vw, vh, ratio
@@ -8,22 +7,22 @@ function Script(){
   Update();
   setInterval(Update, 1000);
   ;
-  function Update(){
-    if (canvas == null){
-      canvas = document.getElementById("unity-canvas")
-      return
-    }
-    console.log("Updating.")
-    vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
-    vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0)
-    ratio = vw/vh
-    if (ratio <= correctRatio){
-      canvas.style.width = vw+"px"
-      canvas.style.height = vw*(1/correctRatio)+"px"
-    }
-    else{
-      canvas.style.height = vh+"px"
-      canvas.style.width = vh*correctRatio+"px"
-    }
+}
+function Update(){
+  if (canvas == null){
+    canvas = document.getElementById("unity-canvas")
+    return
+  }
+  console.log("Updating.")
+  vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
+  vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0)
+  ratio = vw/vh
+  if (ratio <= correctRatio){
+    canvas.style.width = vw+"px"
+    canvas.style.height = vw*(1/correctRatio)+"px"
+  }
+  else{
+    canvas.style.height = vh+"px"
+    canvas.style.width = vh*correctRatio+"px"
   }
 }
