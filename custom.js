6df5625031620e9a3233e905214e5a4dd5ffe5b9
Rasutei <rustingheart@hotmail.com>
2021-08-20 17:06:23 -0300

    Update custom.js

--- custom.js
+++ custom.js
@@ -1,3 +1,4 @@
+console.log("Starting script.")
 var vw, vh, ratio
 var correctRatio = 16/9
 var canvas = document.body.firstElementChild.firstElementChild;
@@ -6,6 +7,7 @@ Update();
 setInterval(Update, 1000);
 ;
 function Update(){
+  console.log("Updating.")
   vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
   vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0)
   ratio = vw/vh
