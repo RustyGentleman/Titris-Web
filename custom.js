3bb5a63da9e724096c28ef9e98a8589a474539f8
Rasutei <rustingheart@hotmail.com>
2021-08-20 17:03:05 -0300

    Create custom.js

---
+++ custom.js
@@ -0,0 +1,20 @@
+var vw, vh, ratio
+var correctRatio = 16/9
+var canvas = document.body.firstElementChild.firstElementChild;
+while (document.getElementById("unity-footer")) document.getElementById("unity-footer").remove();
+Update();
+setInterval(Update, 1000);
+;
+function Update(){
+  vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
+  vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0)
+  ratio = vw/vh
+  if (ratio <= correctRatio){
+    canvas.style.width = vw+"px"
+    canvas.style.height = vw*(1/correctRatio)+"px"
+  }
+  else{
+    canvas.style.height = vh+"px"
+    canvas.style.width = vh*correctRatio+"px"
+  }
+}
