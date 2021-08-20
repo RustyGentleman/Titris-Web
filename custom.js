07d79ef2539b5e85cf36d6447d4d826a6728df96
Rasutei <rustingheart@hotmail.com>
2021-08-20 17:44:21 -0300

    Update custom.js

--- custom.js
+++ custom.js
@@ -1,29 +1,28 @@
-window.addEventListener("load", Script);
-function Script(){
-  console.log("Starting script.")
-  var vw, vh, ratio
-  var correctRatio = 16/9
-  var canvas = document.getElementById("unity-canvas")
-  Update();
-  setInterval(Update, 1000)
-  function Update(){
-    if (canvas == null){
-    console.log("Updating.")
-      canvas = document.getElementById("unity-canvas")
-      return
-    }
-    if (document.getElementById("unity-footer")) document.getElementById("unity-footer").remove();
-    vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
-    vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0)
-    ratio = vw/vh
-    if (ratio <= correctRatio){
-      canvas.style.width = vw+"px"
-      canvas.style.height = vw*(1/correctRatio)+"px"
-    }
-    else{
-      canvas.style.height = vh+"px"
-      canvas.style.width = vh*correctRatio+"px"
-    }
-}
-}
-
+window.addEventListener("load", function(){
+    // ....
+    console.log("Starting script.")
+    var vw, vh, ratio
+    var correctRatio = 16/9
+    var canvas = document.getElementById("unity-canvas")
+    Update();
+    setInterval(Update, 1000)
+    function Update(){
+      if (canvas == null){
+      console.log("Updating.")
+        canvas = document.getElementById("unity-canvas")
+        return
+      }
+      if (document.getElementById("unity-footer")) document.getElementById("unity-footer").remove();
+      vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
+      vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0)
+      ratio = vw/vh
+      if (ratio <= correctRatio){
+        canvas.style.width = vw+"px"
+        canvas.style.height = vw*(1/correctRatio)+"px"
+      }
+      else{
+        canvas.style.height = vh+"px"
+        canvas.style.width = vh*correctRatio+"px"
+      }
+  }
+});
